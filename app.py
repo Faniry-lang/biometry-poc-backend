8442b63d598f84c65689d9b9a327024e3b3d2280
Mazà Faniriniaina <faniryranaivoson167@gmail.com>
2026-09-11 11:58:13 +0300

    update app.py

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
 migrate = Migrate(app, db)
+with app.app_context():
+    db.create_all()
 
 FINGER_LABELS = [
     'left_thumb', 
