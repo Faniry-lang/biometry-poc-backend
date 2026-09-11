e62a4d1e6ba1c601691453313c91e2f0510ee313
Mazà Faniriniaina <faniryranaivoson167@gmail.com>
2026-09-11 12:08:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from operator import and_
+from sqlalchemy import and_
 from flask import Flask, jsonify, request, abort
 from database import db
 from models import Client, Fingerprint
